1f623096fd4a77f70d556d15db3611cc10bee471
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-22 11:47:05 +0300

    Added CSS classes

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -2,11 +2,11 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>March 28th 2023</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Car Insurance</h2>
-                <div>$399</div>
+                <div className="expense-item__price">$399</div>
             </div>
         </div>
     );
