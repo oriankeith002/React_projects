659b223039d14ce4ad1f571a77cd4fed19859dda
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-22 12:45:46 +0300

    Splitting components into sub components

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -1,20 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    //this is purely javascript
-    const month = props.date.toLocaleString('en-US',{month:'long'});
-    const year = props.date.toLocaleString('en-US', {day:'2-digit'});
-    const day = props.date.getFullYear();
-
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
-
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
