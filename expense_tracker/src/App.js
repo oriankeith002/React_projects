38b71edcc12f40f9c2d7b553b67c8ab3f81374ec
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-24 00:10:34 +0300

    Adding form to project

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
 
   return (  
     <div>
-      <h2>Let's get started</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
