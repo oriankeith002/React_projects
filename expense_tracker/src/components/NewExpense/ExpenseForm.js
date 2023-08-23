20d19e775a0d994804ed81eaa23aaed9a7d52cce
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-24 00:30:16 +0300

    Two way binding - clearing data from form fields

--- expense_tracker/src/components/NewExpense/ExpenseForm.js
+++ expense_tracker/src/components/NewExpense/ExpenseForm.js
@@ -33,7 +33,11 @@ const ExpenseForm = () => {
             date: new Date(enteredDate),
         };
 
-        console.log(expenseData);
+        // console.log(expenseData);
+
+        setEnteredTitle(''); // setting it back to empty string
+        setEnteredAmount('');
+        setEnteredDate('');
 
 
     };
@@ -45,7 +49,11 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input
+                        type="text" 
+                        value = {enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
@@ -53,6 +61,7 @@ const ExpenseForm = () => {
                         type="number"
                         min="0.01" 
                         step="0.01" 
+                        value = {enteredAmount}
                         onChange = {amountChangeHandler}
                     />
                 </div>
@@ -62,6 +71,7 @@ const ExpenseForm = () => {
                         type="date" 
                         min="2022-01-01" 
                         max="2022-12-31"
+                        value = {enteredDate}
                         onChange={dateChangeHandler} 
                     />
                 </div>
