d20cf7d8561284f82dad3555a5aa3577c40b0ed9
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-23 23:57:53 +0300

    Working with states

--- expense_tracker/src/components/Expenses/ExpenseItem.js
+++ expense_tracker/src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => { 
 
+    const [title, setNewTitle] = useState(props.title);
+    console.log('Expense item evaluated by React');
+
     const clickHandler = () => {
-        console.log('Clicked!!');
+        setNewTitle('updated!');
+        console.log(title);
     }
 
 
@@ -14,7 +18,7 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
