a46a3e9992ad8424e069ff33f0eb0c8f9c9b2c6c
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-08-24 13:16:36 +0300

    Rendering lists and displaying data in lists

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,20 +1,24 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+// defining external externally , creating sample array of 4 expenses
+const DUMMY_EXPENSES = [
+  {id:'e1', title:'Car Insurance', amount: 294, date: new Date(2023,8,8),},
+  {id:'e2', title:'Phone Insurance', amount: 794, date: new Date(2023,4,4),},
+  {id:'e3', title:'Skin Insurance', amount: 394, date: new Date(2023,7,8),},
+  {id:'e4', title:'Home Insurance', amount: 204, date: new Date(2023,9,8),},
+];
 
 const App = () => {
 
-  // defining external externally , creating sample array of 4 expenses
-  const expenses = [
-    {id:'e1', title:'Car Insurance', amount: 294, date: new Date(2023,8,8),},
-    {id:'e2', title:'Phone Insurance', amount: 794, date: new Date(2023,4,4),},
-    {id:'e3', title:'Skin Insurance', amount: 394, date: new Date(2023,7,8),},
-    {id:'e4', title:'Home Insurance', amount: 204, date: new Date(2023,9,8),},
-  ];
-
+  // we want to display newly added data
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  
   const addExpenseHandler = expense => {
-    console.log('In App.js ');
-    console.log(expense); // data passed via argument
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (  
--- expense_tracker/src/components/Expenses/ExpenseItem.js
+++ expense_tracker/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => { 
 
-    const [title, setNewTitle] = useState(props.title);
-    console.log('Expense item evaluated by React');
-
-    const clickHandler = () => {
-        setNewTitle('updated!');
-        console.log(title);
-    }
-
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
